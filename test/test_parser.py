b9276e79b65e461079e7977ab2c397c15baeec47
bib0x <bib0x.github@bootsect.org>
2023-08-31 16:45:23 +0200

    add tests for fastly

--- test/test_parser.py
+++ test/test_parser.py
@@ -865,5 +865,250 @@ DNSSEC: unsigned
 
         self._parse_and_compare('google.co.bw', data, expected_results)
 
+    def test_com_emails_parse(self):
+        data = """
+   Domain Name: FASTLY.COM
+   Registry Domain ID: 88199966_DOMAIN_COM-VRSN
+   Registrar WHOIS Server: whois.markmonitor.com
+   Registrar URL: http://www.markmonitor.com
+   Updated Date: 2023-07-17T21:00:13Z
+   Creation Date: 2002-07-08T06:13:35Z
+   Registry Expiry Date: 2025-07-08T06:19:11Z
+   Registrar: MarkMonitor Inc.
+   Registrar IANA ID: 292
+   Registrar Abuse Contact Email: abusecomplaints@markmonitor.com
+   Registrar Abuse Contact Phone: +1.2086851750
+   Domain Status: clientDeleteProhibited https://icann.org/epp#clientDeleteProhibited
+   Domain Status: clientTransferProhibited https://icann.org/epp#clientTransferProhibited
+   Domain Status: clientUpdateProhibited https://icann.org/epp#clientUpdateProhibited
+   Domain Status: serverDeleteProhibited https://icann.org/epp#serverDeleteProhibited
+   Domain Status: serverTransferProhibited https://icann.org/epp#serverTransferProhibited
+   Domain Status: serverUpdateProhibited https://icann.org/epp#serverUpdateProhibited
+   Name Server: EDNS1.ULTRADNS.BIZ
+   Name Server: EDNS1.ULTRADNS.COM
+   Name Server: EDNS1.ULTRADNS.NET
+   Name Server: EDNS1.ULTRADNS.ORG
+   Name Server: NS-CLOUD-A1.GOOGLEDOMAINS.COM
+   Name Server: NS-CLOUD-A2.GOOGLEDOMAINS.COM
+   Name Server: NS-CLOUD-A3.GOOGLEDOMAINS.COM
+   Name Server: NS-CLOUD-A4.GOOGLEDOMAINS.COM
+   DNSSEC: unsigned
+   URL of the ICANN Whois Inaccuracy Complaint Form: https://www.icann.org/wicf/
+>>> Last update of whois database: 2023-08-31T14:24:29Z <<<
+
+For more information on Whois status codes, please visit https://icann.org/epp
+
+NOTICE: The expiration date displayed in this record is the date the
+registrar's sponsorship of the domain name registration in the registry is
+currently set to expire. This date does not necessarily reflect the expiration
+date of the domain name registrant's agreement with the sponsoring
+registrar.  Users may consult the sponsoring registrar's Whois database to
+view the registrar's reported date of expiration for this registration.
+
+TERMS OF USE: You are not authorized to access or query our Whois
+database through the use of electronic processes that are high-volume and
+automated except as reasonably necessary to register domain names or
+modify existing registrations; the Data in VeriSign Global Registry
+Services' ("VeriSign") Whois database is provided by VeriSign for
+information purposes only, and to assist persons in obtaining information
+about or related to a domain name registration record. VeriSign does not
+guarantee its accuracy. By submitting a Whois query, you agree to abide
+by the following terms of use: You agree that you may use this Data only
+for lawful purposes and that under no circumstances will you use this Data
+to: (1) allow, enable, or otherwise support the transmission of mass
+unsolicited, commercial advertising or solicitations via e-mail, telephone,
+or facsimile; or (2) enable high volume, automated, electronic processes
+that apply to VeriSign (or its computer systems). The compilation,
+repackaging, dissemination or other use of this Data is expressly
+prohibited without the prior written consent of VeriSign. You agree not to
+use electronic processes that are automated and high-volume to access or
+query the Whois database except as reasonably necessary to register
+domain names or modify existing registrations. VeriSign reserves the right
+to restrict your access to the Whois database in its sole discretion to ensure
+operational stability.  VeriSign may restrict or terminate your access to the
+Whois database for failure to abide by these terms of use. VeriSign
+reserves the right to modify these terms at any time.
+
+The Registry database contains ONLY .COM, .NET, .EDU domains and
+Registrars.
+Domain Name: fastly.com
+Registry Domain ID: 88199966_DOMAIN_COM-VRSN
+Registrar WHOIS Server: whois.markmonitor.com
+Registrar URL: http://www.markmonitor.com
+Updated Date: 2023-07-17T20:04:24+0000
+Creation Date: 2002-07-08T06:13:35+0000
+Registrar Registration Expiration Date: 2025-07-08T00:00:00+0000
+Registrar: MarkMonitor, Inc.
+Registrar IANA ID: 292
+Registrar Abuse Contact Email: abusecomplaints@markmonitor.com
+Registrar Abuse Contact Phone: +1.2086851750
+Domain Status: clientUpdateProhibited (https://www.icann.org/epp#clientUpdateProhibited)
+Domain Status: clientTransferProhibited (https://www.icann.org/epp#clientTransferProhibited)
+Domain Status: clientDeleteProhibited (https://www.icann.org/epp#clientDeleteProhibited)
+Domain Status: serverUpdateProhibited (https://www.icann.org/epp#serverUpdateProhibited)
+Domain Status: serverTransferProhibited (https://www.icann.org/epp#serverTransferProhibited)
+Domain Status: serverDeleteProhibited (https://www.icann.org/epp#serverDeleteProhibited)
+Registry Registrant ID:
+Registrant Name: Domain Administrator
+Registrant Organization: DNStination Inc.
+Registrant Street: 3450 Sacramento Street, Suite 405
+Registrant City: San Francisco
+Registrant State/Province: CA
+Registrant Postal Code: 94118
+Registrant Country: US
+Registrant Phone: +1.4155319335
+Registrant Phone Ext:
+Registrant Fax: +1.4155319336
+Registrant Fax Ext:
+Registrant Email: admin@dnstinations.com
+Registry Admin ID:
+Admin Name: Domain Administrator
+Admin Organization: DNStination Inc.
+Admin Street: 3450 Sacramento Street, Suite 405
+Admin City: San Francisco
+Admin State/Province: CA
+Admin Postal Code: 94118
+Admin Country: US
+Admin Phone: +1.4155319335
+Admin Phone Ext:
+Admin Fax: +1.4155319336
+Admin Fax Ext:
+Admin Email: admin@dnstinations.com
+Registry Tech ID:
+Tech Name: Domain Administrator
+Tech Organization: DNStination Inc.
+Tech Street: 3450 Sacramento Street, Suite 405
+Tech City: San Francisco
+Tech State/Province: CA
+Tech Postal Code: 94118
+Tech Country: US
+Tech Phone: +1.4155319335
+Tech Phone Ext:
+Tech Fax: +1.4155319336
+Tech Fax Ext:
+Tech Email: admin@dnstinations.com
+Name Server: edns1.ultradns.biz
+Name Server: edns1.ultradns.com
+Name Server: ns-cloud-a3.googledomains.com
+Name Server: edns1.ultradns.net
+Name Server: ns-cloud-a2.googledomains.com
+Name Server: ns-cloud-a1.googledomains.com
+Name Server: ns-cloud-a4.googledomains.com
+Name Server: edns1.ultradns.org
+DNSSEC: unsigned
+URL of the ICANN WHOIS Data Problem Reporting System: http://wdprs.internic.net/
+>>> Last update of WHOIS database: 2023-08-31T14:24:19+0000 <<<
+
+For more information on WHOIS status codes, please visit:
+  https://www.icann.org/resources/pages/epp-status-codes
+
+If you wish to contact this domain’s Registrant, Administrative, or Technical
+contact, and such email address is not visible above, you may do so via our web
+form, pursuant to ICANN’s Temporary Specification. To verify that you are not a
+robot, please enter your email address to receive a link to a page that
+facilitates email communication with the relevant contact(s).
+
+Web-based WHOIS:
+  https://domains.markmonitor.com/whois
+
+If you have a legitimate interest in viewing the non-public WHOIS details, send
+your request and the reasons for your request to whoisrequest@markmonitor.com
+and specify the domain name in the subject line. We will review that request and
+may ask for supporting documentation and explanation.
+
+The data in MarkMonitor’s WHOIS database is provided for information purposes,
+and to assist persons in obtaining information about or related to a domain
+name’s registration record. While MarkMonitor believes the data to be accurate,
+the data is provided "as is" with no guarantee or warranties regarding its
+accuracy.
+
+By submitting a WHOIS query, you agree that you will use this data only for
+lawful purposes and that, under no circumstances will you use this data to:
+  (1) allow, enable, or otherwise support the transmission by email, telephone,
+or facsimile of mass, unsolicited, commercial advertising, or spam; or
+  (2) enable high volume, automated, or electronic processes that send queries,
+data, or email to MarkMonitor (or its systems) or the domain name contacts (or
+its systems).
+
+MarkMonitor reserves the right to modify these terms at any time.
+
+By submitting this query, you agree to abide by this policy.
+
+MarkMonitor Domain Management(TM)
+Protecting companies and consumers in a digital world.
+
+Visit MarkMonitor at https://www.markmonitor.com
+Contact us at +1.8007459229
+In Europe, at +44.02032062220
+----
+"""
+
+        expected_results = {
+                'domain_name': ['FASTLY.COM', 'fastly.com'], 
+                'registrar': 'MarkMonitor, Inc.', 
+                'whois_server': 'whois.markmonitor.com', 
+                'referral_url': None, 
+                'updated_date': [ 
+                    datetime.datetime(2023, 7, 17, 21, 0, 13), 
+                    datetime.datetime(2023, 7, 17, 20, 4, 24, tzinfo=datetime.timezone.utc) 
+                ], 
+                'creation_date': [
+                    datetime.datetime(2002, 7, 8, 6, 13, 35), 
+                    datetime.datetime(2002, 7, 8, 6, 13, 35, tzinfo=datetime.timezone.utc)
+                ], 
+                'expiration_date': [
+                    datetime.datetime(2025, 7, 8, 6, 19, 11), 
+                    datetime.datetime(2025, 7, 8, 0, 0, tzinfo=datetime.timezone.utc)
+                ], 
+                'name_servers': [
+                    'EDNS1.ULTRADNS.BIZ', 
+                    'EDNS1.ULTRADNS.COM', 
+                    'EDNS1.ULTRADNS.NET', 
+                    'EDNS1.ULTRADNS.ORG', 
+                    'NS-CLOUD-A1.GOOGLEDOMAINS.COM', 
+                    'NS-CLOUD-A2.GOOGLEDOMAINS.COM', 
+                    'NS-CLOUD-A3.GOOGLEDOMAINS.COM', 
+                    'NS-CLOUD-A4.GOOGLEDOMAINS.COM', 
+                    'edns1.ultradns.biz', 
+                    'edns1.ultradns.com', 
+                    'ns-cloud-a3.googledomains.com', 
+                    'edns1.ultradns.net', 
+                    'ns-cloud-a2.googledomains.com', 
+                    'ns-cloud-a1.googledomains.com', 
+                    'ns-cloud-a4.googledomains.com', 
+                    'edns1.ultradns.org'
+                ], 
+                'status': [
+                    'clientDeleteProhibited https://icann.org/epp#clientDeleteProhibited', 
+                    'clientTransferProhibited https://icann.org/epp#clientTransferProhibited', 
+                    'clientUpdateProhibited https://icann.org/epp#clientUpdateProhibited', 
+                    'serverDeleteProhibited https://icann.org/epp#serverDeleteProhibited', 
+                    'serverTransferProhibited https://icann.org/epp#serverTransferProhibited', 
+                    'serverUpdateProhibited https://icann.org/epp#serverUpdateProhibited', 
+                    'clientUpdateProhibited (https://www.icann.org/epp#clientUpdateProhibited)', 
+                    'clientTransferProhibited (https://www.icann.org/epp#clientTransferProhibited)', 
+                    'clientDeleteProhibited (https://www.icann.org/epp#clientDeleteProhibited)', 
+                    'serverUpdateProhibited (https://www.icann.org/epp#serverUpdateProhibited)', 
+                    'serverTransferProhibited (https://www.icann.org/epp#serverTransferProhibited)', 
+                    'serverDeleteProhibited (https://www.icann.org/epp#serverDeleteProhibited)'
+                ], 
+                'emails': [
+                    'abusecomplaints@markmonitor.com', 
+                    'admin@dnstinations.com', 
+                    'whoisrequest@markmonitor.com'
+                ], 
+                'dnssec': 'unsigned', 
+                'name': 'Domain Administrator', 
+                'org': 'DNStination Inc.', 
+                'address': '3450 Sacramento Street, Suite 405', 
+                'city': 'San Francisco', 
+                'state': 'CA', 
+                'registrant_postal_code': '94118', 
+                'country': 'US'
+        }
+
+        self._parse_and_compare('fastly.com', data, expected_results)
+
+
 if __name__ == '__main__':
     unittest.main()
